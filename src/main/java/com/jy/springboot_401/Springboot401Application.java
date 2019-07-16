d044246efc4345296a7c751941a4c5bcf7cae775
Justin <justinyang1000@hotmail.com>
2019-07-16 10:32:01 -0400

    Initial commit

---
+++ src/main/java/com/jy/springboot_401/Springboot401Application.java
@@ -0,0 +1,13 @@
+package com.jy.springboot_401;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot401Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot401Application.class, args);
+    }
+
+}
